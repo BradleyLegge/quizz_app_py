26bb6cc292b875923b10741e1f3e5679bef5a9ab
BradleyLegge <bmlegge12@gmail.com>
2025-08-27 19:48:07 -0400

    added loop to read questions

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-file_path = "q_and_a.txt"
+import json
 
-TOTAL_QUESTIONS = 10
+file_path = "q_and_a.json"
+
+TOTAL_QUESTIONS = 3
 questions = {}
 score = 0
 
 try:
     with open(file_path, 'r') as file:
-        for line in file:
-            key, value = line.strip().split(":", 1)
-            questions[key.strip()] = value.strip()
+        questions = json.load(file)
 except FileNotFoundError:
     print(f"Error: The file '{file_path}' was not found.")
 except Exception as e:
     print(f"An error occurred: {e}")
 
-print(questions["question"])
-print(questions["choices"])
-user_input = input("Answer: ==> ")
+for question in questions:
+    print(question["question"])
+    print(question["choices"])
+    user_input = input("Choice your answer: ==> ")
 
-# print(questions["answer"])
+    if user_input == question["answer"]:
+        print("Correct!")
+        score += 1
+    else: print("Incorrect")
 
-if user_input.lower() == questions["answer"]:
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect!")
-
-print(f"{score} out of {TOTAL_QUESTIONS}")
+print(f"You got {score} out of {TOTAL_QUESTIONS} correct!")
+print(f"Your score is {(score/TOTAL_QUESTIONS)*100:.2f}")
